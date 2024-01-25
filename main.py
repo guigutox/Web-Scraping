89abfbfbc8f3996b75b1d5dbe9de94e473ae940c
Guilherme Augusto Fernandes <69806054+guigutox@users.noreply.github.com>
2024-01-25 18:40:30 -0300

    Feature measurement conversion

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import re
 import json
 from bs4 import BeautifulSoup
 from unidecode import unidecode
+conversions=json.load(open('conversions.json'))
 
 receita = 'arroz+branco'
 lista_ingredientes = []
 
 url = f'https://receitinhas.com.br/?s={receita}'
+
 
 r = requests.get(url)
 
@@ -30,14 +32,35 @@ if requestReceita.status_code == 200:
     soup = BeautifulSoup(requestReceita.text, 'html.parser')
     ingredientes = soup.find('ul', class_='recipe-content__steps')
     for ingrediente in ingredientes.find_all('li'):  # Iterar sobre cada item da lista
-        texto_limpo = unidecode(ingrediente.text.replace('Check', '').replace('½', '0,5'))
-        texto_sem_parenteses = re.sub(r'\([^)]*\)', '', texto_limpo)
-        lista_ingredientes.append(texto_sem_parenteses)
+        texto_limpo = unidecode(ingrediente.text.replace('Check', '').replace('½', '0.5').replace('⅛', '0.12').replace('¼', '0.25').replace('¾', '0.75'))
+        texto_sem_parenteses = re.sub(r'\(([^)]*)\)', r'\1', texto_limpo)
+        numero = texto_sem_parenteses.split(" ")
+        print(numero[0])
+        if(numero[1]=='xicara' or numero[1]=='xicaras'):
+            quantidade = float(numero[0])*float(conversions['xicara']['ml'])
+        elif(numero[1]=='colher' or numero[1]=='colheres'):
+            if(numero[2]=='sopa'):
+                quantidade = float(numero[0])*float(conversions['colher sopa']['ml'])
+            elif(numero[2]=='cha'):
+                quantidade = float(numero[0])*float(conversions['colher cha']['ml'])
+        else:
+            quantidade = float(numero[0])
+        lista_ingredientes.append({"ingrediente": texto_sem_parenteses, "quantidade": quantidade })  # Adicionando cada ingrediente como um dicionário separado
 else:
     print('Requisição falhou')
 
-for x in lista_ingredientes:
-    print(x)
 
-saida = json.dumps(lista_ingredientes)
-print("Lista convertida: ", saida)
+# Montando o corpo com as informações tratadas
+body = {
+    "url_do_item": href_do_item,
+    "ingredientes": lista_ingredientes
+}
+
+# Convertendo o dicionário em JSON com formatação legível
+body_json = json.dumps(body, indent=4)
+
+
+
+
+print("Body JSON:")
+print(body_json)
